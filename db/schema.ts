18c57334c6b22f351e18f2f0987f7b520c634342
Tim Benniks <tbenniks@gmail.com>
2026-06-26 14:05:43 +0200

    Initial commit from turbo-project

---
+++ db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+// Add your schema definitions here.
+// Example:
+//
+// export const users = pgTable("users", {
+//   id: serial("id").primaryKey(),
+//   name: text("name").notNull(),
+//   email: text("email").notNull().unique(),
+//   createdAt: timestamp("created_at").defaultNow().notNull(),
+// });
